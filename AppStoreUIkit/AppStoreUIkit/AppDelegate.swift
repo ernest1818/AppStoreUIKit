ea0d1ad705e074c3f7d366cafa586e1422caf2ba
Ernest Avagovich <erni1937@gmail.com>
2022-10-06 02:04:42 +0400

    initial commit

---
+++ AppStoreUIkit/AppStoreUIkit/AppDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.swift
+//  AppStoreUIkit
+//
+//  Created by Ernest Avagovich on 06.10.2022.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
---
+++ AppStoreUIkit/AppStoreUIkit/SceneDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  SceneDelegate.swift
+//  AppStoreUIkit
+//
+//  Created by Ernest Avagovich on 06.10.2022.
+//
+
+import UIKit
+
+final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    var window: UIWindow?
+    func scene(_ scene: UIScene,
+               willConnectTo session: UISceneSession,
+               options connectionOptions: UIScene.ConnectionOptions) {
+        guard let applicationScene = (scene as? UIWindowScene) else { return }
+        window?.windowScene = applicationScene
+        window?.makeKeyAndVisible()
+        
+        let startViewController = ViewController()
+        let navigationController = UINavigationController(rootViewController: startViewController)
+        window?.rootViewController = navigationController
+        
+    }
+}
